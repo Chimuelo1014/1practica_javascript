f1c089dc0d76d8b6d31fa3bb6c11dbe271879b42
Samuel Quintero <quinterosanchezsamuel1@gmail.com>
2025-06-16 21:51:51 -0500

    Create practica.js

---
+++ practica.js
@@ -0,0 +1,36 @@
+//1
+
+let name = "Samuel"
+let age = 18
+let height = 1.77
+let country = "Medellin"
+let pet = true
+
+
+//2
+//Nombre
+name = "santiago"
+console.log(typeof(name))
+//edad
+age = 19
+console.log(typeof(age)) 
+//altura
+height = 1.80
+console.log(typeof(height)) 
+
+
+
+
+//3
+
+
+let friends_facebook = 20 
+let friends_instagram = 20 
+
+let total_friends = friends_facebook+friends_instagram 
+console.log(total_friends)
+
+
+//4
+let age = prompt("Ingrese su edad: "); 
+let age_num = Number(int);
