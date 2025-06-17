a54fe515246312e5cd1755d5ca016867b784c19e
Samuel Quintero <quinterosanchezsamuel1@gmail.com>
2025-06-17 15:56:22 -0500

    cambios hasta el punto 6

--- practica.js
+++ practica.js
@@ -1,36 +1,76 @@
-//1
+//1 Declara variables que representen tu nombre, edad, estatura en metros, ciudad y si tienes mascota (sí/no)
 
-let name = "Samuel"
-let age = 18
-let height = 1.77
-let country = "Medellin"
-let pet = true
+// let name = "Samuel"
+// let age = 18
+// let height = 1.77
+// let country = "Medellin"
+// let pet = true
 
 
-//2
+//2 Cambia los valores y explica qué tipo de dato representa cada uno (Muestra en consola el tipo de dato).
 //Nombre
-name = "santiago"
-console.log(typeof(name))
-//edad
-age = 19
-console.log(typeof(age)) 
-//altura
-height = 1.80
-console.log(typeof(height)) 
+// name = "santiago"
+// console.log(typeof(name))
+// //edad
+// age = 19
+// console.log(typeof(age)) 
+// //altura
+// height = 1.80
+// console.log(typeof(height)) 
 
 
 
 
-//3
+//3 Declara dos variables que se sumen (por ejemplo, cantidad de amigos en dos redes sociales) y una que contenga el resultado de una concatenación
 
 
-let friends_facebook = 20 
-let friends_instagram = 20 
+// let friends_facebook = 20 
+// let friends_instagram = 20 
 
-let total_friends = friends_facebook+friends_instagram 
-console.log(total_friends)
+// let total_friends = friends_facebook+friends_instagram 
+// console.log(total_friends)
 
 
-//4
-let age = prompt("Ingrese su edad: "); 
-let age_num = Number(int);
+//4 Una persona quiere entrar a una discoteca. Solo puede si tiene más de 18 años. Muestra un mensaje dependiendo si entra o no.
+// let age = prompt("Ingrese su edad: "); 
+// let age_num = Number(age);
+
+// if(age_num>=18){
+//     prompt("El ususario es mayor de edad");
+// } else{
+//     prompt("El ususario es menor de edad");} 
+
+
+
+//5 Una tienda online da descuentos si el cliente compra más de 3 productos. Evalúa si aplica descuento
+// let amount = prompt("Ingrese la cantidad de productos: "); 
+// let amount_num = Number(amount);
+
+// if(amount_num>3){
+//     prompt("Si aplica descuento");
+// } else{
+//     prompt("No aplica descuento");} 
+
+
+
+//6 Un estudiante recibe una nota del 0 al 5. Crea condiciones para saber si reprueba, aprueba o tiene excelente desempeño.
+
+// let calification = prompt("Ingrese la nota del estudiante: "); 
+// let calification_num = Number(calification);
+// if(calification_num>5 || calification_num<0){
+//     prompt("Ingrese un numero valido");
+// } else if(calification_num>4 && calification _num<=5){
+//     prompt("Excelente desempeño");
+//    } else if(calification_num > 3 && calification_num <=4){
+//         prompt("aprobo");
+
+//    }else if(calification_num < 3{
+//         prompt("reprobo");
+
+//    } 
+
+
+
+
+
+
